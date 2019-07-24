7568a1f1167bd4adaa14d7cc5e36ae017b81dfe0
sherlockzhan <zxjnchu@163.com>
2019-07-24 23:31:34 +0800

    添加相关代码

--- zxj-pay/zxj-pay-interface/src/main/java/com/zxj/pay/service/PayService.java
+++ zxj-pay/zxj-pay-interface/src/main/java/com/zxj/pay/service/PayService.java
@@ -1,5 +1,11 @@
 package com.zxj.pay.service;
 
+import java.util.Map;
+
 public interface PayService {
 
+	/**
+	 * 需求：获取二维码地址
+	 */
+	public Map<String, Object> createNative();
 }
--- zxj-pay/zxj-pay-service/src/main/java/com/zxj/pay/service/impl/PayServiceImpl.java
+++ zxj-pay/zxj-pay-service/src/main/java/com/zxj/pay/service/impl/PayServiceImpl.java
@@ -1,7 +1,17 @@
 package com.zxj.pay.service.impl;
 
+import java.util.Map;
+
+import com.alibaba.dubbo.config.annotation.Service;
 import com.zxj.pay.service.PayService;
 
+@Service
 public class PayServiceImpl implements PayService{
+
+	@Override
+	public Map<String, Object> createNative() {
+		
+		return null;
+	}
 
 }
